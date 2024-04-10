f09cc8ad77d3ba9b8646e8cf79be3500ff776217
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2024-04-10 22:53:52 +0200

    production test 18

--- config.php
+++ config.php
@@ -8,10 +8,5 @@ error_reporting(E_ALL);
 require_once 'vendor/autoload.php';
 use Dotenv\Dotenv as Dotenv;
 
-echo realpath('.\\');
-echo realpath('./');
-echo realpath('../');
-echo realpath('/');
-
-$dotenv = Dotenv::createImmutable(realpath('./'));
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
