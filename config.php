426a31d5c3fb2bace99c7362f2b57c0af278c0db
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2024-04-10 23:47:11 +0200

    production test 19

--- config.php
+++ config.php
@@ -6,7 +6,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'vendor/autoload.php';
-use Dotenv\Dotenv as Dotenv;
+//use Dotenv\Dotenv as Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+
+var_dump($_ENV);
+//$dotenv = Dotenv::createImmutable(__DIR__);
+//$dotenv->load();
