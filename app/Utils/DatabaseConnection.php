3bdc69fa800ad01b12d7919d5dd05dbd1db7ef53
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2024-04-10 20:12:51 +0200

    added .env config

--- app/Utils/DatabaseConnection.php
+++ app/Utils/DatabaseConnection.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once 'app/config.php';
+require_once 'config.php';
+require_once 'vendor/autoload.php';
+
+use Dotenv\Dotenv as Dotenv;
 
 class DatabaseConnection
 {
@@ -9,12 +12,14 @@ class DatabaseConnection
 
     public function __construct()
     {
+        $dotenv = Dotenv::createImmutable(ROOT_DIR);
+        $dotenv->load();
         $this->connection = $this->connect();
     }
 
     private function connect() {
         try {
-            $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+            $connection = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
             if ($connection->connect_error) {
                 throw new Exception("Error connecting to the database: " . $connection->connect_error);
             }
